de5d439c5ff24ad3bbb30c1e361f88dfa1621745
diegoc0rrales <drcorrales@uamv.edu.ni>
2025-04-22 11:23:33 -0600

    Ejercicio en Python

---
+++ actividad4/ej1.py
@@ -0,0 +1,9 @@
+inicio = int(input("Dime el primer valor: "))
+fin= int(input("Dime el valor final: "))
+
+numero = int (input("Dime un numero: "))
+
+if numero >= inicio and numero <= fin:
+    print("El número es válido")
+else:
+    print("El número no es válido")
