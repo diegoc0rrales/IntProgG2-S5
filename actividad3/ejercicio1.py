490997e09e8633fe6361ea22c0fb79eb2fcee4fe
diegoc0rrales <drcorrales@uamv.edu.ni>
2025-04-22 08:21:16 -0600

    compuse todo

---
+++ actividad3/ejercicio1.py
@@ -0,0 +1,11 @@
+calificacion1 = int(input("Ingrese la calificación 1: "))
+calificacion2 = int(input("Ingrese la calificación 2: "))
+calificacion3 = int(input("Ingrese la calificación 3: "))
+
+suma = calificacion1 + calificacion2 + calificacion3
+promedio= suma / 3
+
+print(f"""Calificación 1: {calificacion1:>3}
+Calificación 2: {calificacion2:>3}
+Calificación 3: {calificacion3:>3}
+{"Promedio:":<15} {promedio:>3.0f}""")
