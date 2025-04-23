1e57b2bc5b252537353ae32a824cedf8b573c254
diegoc0rrales <drcorrales@uamv.edu.ni>
2025-04-23 11:58:21 -0600

    Ejercicio2

---
+++ actividad4/ej2.py
@@ -0,0 +1,14 @@
+import datetime as dt
+#fecha_ingreso = input(Fecha Ingreso YYYY-MM-DD)
+
+fecha_ingreso = input("Fecha Ingreso YYYY-MM-DD")
+fecha_ingreso = dt.datetime.strptime(fecha_ingreso,"%Y-%m-%d")
+fecha_actual = dt.datetime.now()
+
+dias= (fecha_actual - fecha_ingreso).days
+print(fecha_actual)
+print(fecha_ingreso)
+print("dias ", dias)
+
+if dias > 30:
+    print("Cuenta inactiva")
