8961cc68cabd9155a18af68b2e8a08c1e8484aa1
diegoc0rrales <drcorrales@uamv.edu.ni>
2025-04-25 08:24:20 -0600

    Ejercicio 3

---
+++ actividad5/ej3.py
@@ -0,0 +1,19 @@
+dia=int(input("Escribe el dia de la semana en forma de numero(1-7): "))
+
+
+if dia==1:
+    print("Lunes") 
+elif dia ==2:
+    print("Martes")
+elif dia ==3:
+    print("Miércoles")
+elif dia ==4:
+    print("Jueves")
+elif dia ==5:
+    print("Viernes")
+elif dia ==6:
+    print("Sábado")
+elif dia ==7:
+    print("Domingo")
+else:
+    print("Error. Número no válido")
